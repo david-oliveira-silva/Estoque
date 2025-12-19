b80dfd9a15f2268606bd0eb3467c06af9e45eeb3
davidOiveira <143909652+davidOiveira@users.noreply.github.com>
2025-12-18 21:54:55 -0300

    Passando validações de produto para métodos de extensão

---
+++ DOMAIN/Extensoes/ProdutoExtensao.cs
@@ -0,0 +1,31 @@
+﻿
+using DOMAIN.Model;
+
+namespace DOMAIN.Extensions
+{
+    public  static class ProdutoExtensao
+    {
+
+        public static void ValidarProduto(this ProdutoModel produto)
+        {
+            if (string.IsNullOrWhiteSpace(produto.NomeProduto))
+            {
+                throw new Exception("Nome não pode ser vazio");
+            }
+            if (produto.ValorProduto < 0)
+            {
+                throw new Exception("Valor não pode ser negativo");
+            }
+
+        }
+
+        public static void ValidarExistencia(this ProdutoModel produto) {
+
+            if (produto == null || produto.CodigoProduto <= 0)
+            {
+                throw new Exception("Produto inválido ou não encontrado para esta operação.");
+            }
+          
+        }
+    }
+}
